f87fe57ef040d057100180f41f2c550b4fc601e0
juan <juanosorio1124226@correo.itm.edu.co>
2025-08-21 09:20:38 -0500

    proy

--- appconsola/Program.cs
+++ appconsola/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-
+public class Dueños
+{
+    public int? id;
+    public string? nombre;
+}
+public class Vehiculos
+{
+    public int? llantas;
+    public string? placa;
+    public decimal kilometraje;
+    public bool anda;
+    public DateTime fecha_compra;
+    public Dueños Dueño;
+}
