9f9411661f46dab35b64a84a78785d4136bf900a
juan <juanosorio1124226@correo.itm.edu.co>
2025-08-22 06:58:57 -0500

    proy

--- appconsola/Program.cs
+++ appconsola/Program.cs
@@ -4,16 +4,28 @@ using System.Data.Common;
 Console.WriteLine("Hello, World!");
 
 Personas persona = new Personas();
+var persona1 = new Personas();
+Estudiantes persona2 = new Estudiantes();
+
 persona.id = 1;
 persona.nombre = "carlos";
 persona.estatura = 1.75m;
 persona.vivo = false;
 persona.fecha = DateTime.Now;
-persona.Estado = new Estados() {id=1, nombre = "viudo"};
+persona.Estado = new Estados() { id = 1, nombre = "viudo" };
+persona.videojuegos = new List<Videojuegos>();
+//persona.videojuegos.Add(new)
+
 
 public class Estados
 {
     public int? id;
+    public string? nombre;
+}
+
+public class Videojuegos : Personas
+{
+    public int id;
     public string? nombre;
 }
 public class Personas
@@ -24,6 +36,7 @@ public class Personas
     public bool vivo;
     public DateTime fecha;
     public Estados? Estado;
+    public List<Videojuegos>? videojuegos;
 }
 
 public class Estudiantes : Personas
